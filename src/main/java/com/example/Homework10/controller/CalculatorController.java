70ddf7a45d0bc2e55bd7ee7d0af586170d7770c8
Denis Siliukov <abhem21rus@mail.ru>
2023-12-20 16:05:13 +0300

    Внесены правки.

--- src/main/java/com/example/Homework10/controller/CalculatorController.java
+++ src/main/java/com/example/Homework10/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package com.example.Homework10.controller;
 
+import com.example.Homework10.exception.CalculatorRuntimeException;
 import com.example.Homework10.service.CalculatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,22 +18,27 @@ public class CalculatorController {
     }
     @GetMapping(path = "/plus")
     public String plusOperation(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2) {
+        if(num1 == null || num2 == null) throw new CalculatorRuntimeException("Параметры не поданы");
         return calculatorService.plus(num1, num2);
     }
     @GetMapping(path = "/minus")
     public String minusOperation(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2) {
+        if(num1 == null || num2 == null) throw new CalculatorRuntimeException("Параметры не поданы");
         return calculatorService.minus(num1, num2);
     }
     @GetMapping(path = "/multiply")
     public String multiplyOperation(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2) {
+        if(num1 == null || num2 == null) throw new CalculatorRuntimeException("Параметры не поданы");
         return calculatorService.multiply(num1, num2);
     }
     @GetMapping(path = "/divide")
     public String divideOperation(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2) {
+        if(num1 == null || num2 == null) throw new CalculatorRuntimeException("Параметры не поданы");
+        if(num2 == 0) throw new CalculatorRuntimeException("На ноль делить нельзя");
         return calculatorService.divide(num1, num2);
     }
     @GetMapping
     public String greetingOperation(){
-        return calculatorService.greeting();
+        return "Добро пожаловать в калькулятор";
     }
 }
--- src/main/java/com/example/Homework10/service/CalculatorService.java
+++ src/main/java/com/example/Homework10/service/CalculatorService.java
@@ -5,6 +5,5 @@ public interface CalculatorService {
     String plus(Double num1, Double num2);
     String minus(Double num1, Double num2);
     String multiply(Double num1, Double num2);
-    String divide(Double num1, Double num2) throws CalculatorRuntimeException;
-    String greeting();
+    String divide(Double num1, Double num2);
 }
